5256052476ea274b06946216cef2b2567d4df71e
GB <gbourel@gmail.com>
2023-11-18 10:24:38 +0300

    Update local debug domain

--- public/js/config.js
+++ public/js/config.js
@@ -1,11 +1,12 @@
 
-export const VERSION = 'v0.8.4';
+export const VERSION = 'v0.9.0';
 
 const host = window.location.host;
-export const dev = host.startsWith('localhost') || host.startsWith('ileauxsciences.test');
+export const dev = host.startsWith('localhost') || host.startsWith('nsix.test');
 export const debug = (dev || location.href.match('#debug')) ? console.info : () => {};
 
-export const NSIX_LOGIN_URL = dev ?  'http://ileauxsciences.test:4200/connexion' : 'http://app.nsix.fr/connexion'
-export const LCMS_URL = dev ? 'http://dev.ileauxsciences.test:9976' : 'https://webamc.nsix.fr';
-export const WS_URL = dev ? 'ws://dev.ileauxsciences.test:9976' : 'wss://webamc.nsix.fr';
-export const COOKIE_DOMAIN = dev ? '.ileauxsciences.test' : '.nsix.fr';
+export const AUTH_COOKIE = 'neossot';
+export const NSIX_LOGIN_URL = dev ?  'http://nsix.test:5173/login' : 'https://www.nsix.fr/login'
+export const LCMS_URL = dev ? 'http://nsix.test:5000/api' : 'https://lcms.nsix.fr/api';
+export const WS_URL = dev ? 'ws://nsix.test:5001' : 'wss://lcms.nsix.fr';
+export const COOKIE_DOMAIN = dev ? '.nsix.test' : '.nsix.fr';
